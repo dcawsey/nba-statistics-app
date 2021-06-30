b04a6de8fb06039f81d32a3688134eb850e3d34d
Dylan Cawsey <dylancawsey@gmail.com>
2021-06-30 19:55:51 +1000

    Parcel typescript react config

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './assets/styles.scss';
+
+ReactDOM.render(<p className="test">Hello World!</p>, document.getElementById('root'));
